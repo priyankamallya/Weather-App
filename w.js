f47bf37e12355ab7753380faae783f3f3bf537e0
priyankamallya <120215272+priyankamallya@users.noreply.github.com>
2024-01-23 17:01:26 +0530

    changes

--- w.js
+++ w.js
@@ -16,35 +16,38 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       var data = await response.json();
-
+      console.log(data);
       document.querySelector(".city").innerHTML = data.name;
       document.querySelector(".temp").innerHTML =
         Math.round(data.main.temp) + "°C";
       document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
       document.querySelector(".wind").innerHTML = data.wind.speed + " Kmph";
 
-      if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "images/clouds.png";
-        document.querySelector(".card").style.background =
-          "linear-gradient(155deg,#696974,#6487aa)";
-      } else if (data.weather[0].main == "Clear") {
+      if (data.weather[0].main == "Clear") {
         weatherIcon.src = "images/clear.png";
         document.querySelector(".card").style.background =
           "linear-gradient(155deg,#f5eb8c,#3c88fa)";
+      } else if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png";
+        document.querySelector(".card").style.background =
+          "linear-gradient(145deg,#ffd76aed,#adafb0);";
       } else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "images/rain.png";
         document.querySelector(".card").style.background =
-          "linear-gradient(155deg,#f5eb8c,#3c88fa)";
+          "linear-gradient(155deg,#a8acaeed,#0591dd)";
       } else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "images/drizzle.png";
         document.querySelector(".card").style.background =
-          "linear-gradient(155deg,#4378c2,#9acaf9)";
+          "linear-gradient(155deg,#a8acaeed,#8acdf2)";
       } else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "mist/rain.png";
+        weatherIcon.src = "images/mist.png";
         document.querySelector(".card").style.background =
-          "linear-gradient(155deg,#76aaf2,#387bbf)";
+          "linear-gradient(155deg,#a8acaeed,#8acdf2)";
+      } else if (data.weather[0].main == "Snow") {
+        weatherIcon.sec = "images/snow.png";
+        document.querySelector(".card").style.background =
+          "linear-gradient(155deg,#ffffffed,#969899)";
       }
-
       document.querySelector(".weather").style.display = "block";
     } catch (error) {
       console.error("Error fetching data:", error.message);
